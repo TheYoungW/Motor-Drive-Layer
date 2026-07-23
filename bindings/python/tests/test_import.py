8e719107873dbd23a2b271c7dc4ca837d2d0fcdf
TheYoungW <2894156147@qq.com>
2026-07-23 16:55:36 +0800

    Release motor-drive-layer 0.5.4

--- bindings/python/tests/test_import.py
+++ bindings/python/tests/test_import.py
@@ -5,4 +5,4 @@ def test_import_symbols() -> None:
     assert Controller is not None
     assert Mode.MIT.value == 1
     assert MotorState is not None
-    assert get_version() == "0.5.3"
+    assert get_version() == "0.5.4"
